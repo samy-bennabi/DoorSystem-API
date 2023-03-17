3ff4ac5ae6f71651e201753a456c1211035e1bca
Samy <99757181+samy-bennabi@users.noreply.github.com>
2023-03-17 12:00:47 -0400

    user routes and ctrl routes and ctrl functions to add user and authenticate with hashed passwords untested

--- app/Http/Controllers/userCtrl.php
+++ app/Http/Controllers/userCtrl.php
@@ -6,5 +6,36 @@ use Illuminate\Http\Request;
 
 class userCtrl extends Controller
 {
+    public function authenticate(Request $req){
+        try{
+        $req->validate([
+            'email'=>['required', 'string',  'min:3', 'max:100'],
+            'password'=> ['required', 'string',  'min:6', 'max:100']
+        ]);
+        }catch(Error $err){return [false,'Fill all fields']; }
+
+        $user = User::firstwhere('email', $req->email);
+
+        if($user == null or !(Hash::check($req->password, $user->password))){
+            return [false, 'Incorrect username or password'];
+        }else{
+            return true;
+        }
+    }
+
+    public function add(Request $req){
+        try{
+            $req->validate([
+                'name'=>['required', 'string',  'min:3', 'max:100'],
+                'email'=>['required', 'string',  'min:3', 'max:100'],
+                'password'=> ['required', 'string',  'min:6', 'max:100']
+            ]);
+        }catch(Error $err){return [false,'Fill all fields']; }
     
+        $user = new User();
+        $user->name = $req->name;
+        $user->email=$req->email;
+        $user->password=Hash::make($req->password);
+        $user->save();
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\cardCtrl;
+use App\Http\Controllers\userCtrl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +21,7 @@ Route::get('card/all', [cardCtrl::class, 'getall']);
 Route::post('card/check', [cardCtrl::class, 'check']);
 Route::post('card/add', [cardCtrl::class, 'add']);
 
+//user
+Route::post('user/auth', [userCtrl::class, 'authentication']);
+Route::post('user/add', [userCtrl::class, 'add']);
 //Route::middleware('auth:sanctum')->get('/user', function (Request $request) { return $request->user();});
