5568bfe75ad27675d671128e698fbab828dee3a5
Samy <99757181+samy-bennabi@users.noreply.github.com>
2023-03-23 11:03:59 -0400

    code cleanup tabs and one-liners

--- app/Http/Controllers/logCtrl.php
+++ app/Http/Controllers/logCtrl.php
@@ -13,19 +13,11 @@ class logCtrl extends Controller
         $log->save();
     }
 
-    public function getAll(){
-        return Log::all();
-    }
+    public function getAll(){ return Log::all(); }
 
-    public function getPastWeek(){
-        return Log::where('created_at', '>=', now()->subWeek())->get();
-    }
+    public function getPastWeek(){ return Log::where('created_at', '>=', now()->subWeek())->get(); }
 
-    public function getPastMonth(){
-        return Log::where('created_at', '>=', now()->subMonth())->get();
-    }
+    public function getPastMonth(){ return Log::where('created_at', '>=', now()->subMonth())->get(); }
 
-    public function getPastYear(){
-        return Log::where('created_at', '>=', now()->subYear())->get();
-    }
+    public function getPastYear(){ return Log::where('created_at', '>=', now()->subYear())->get(); }
 }
