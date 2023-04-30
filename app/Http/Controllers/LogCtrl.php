6142a3db319fd31348170a5d5781ea1ca0fa200c
Samy <99757181+samy-bennabi@users.noreply.github.com>
2023-04-30 04:01:37 -0400

    hotfix db

--- app/Http/Controllers/LogCtrl.php
+++ app/Http/Controllers/LogCtrl.php
@@ -24,14 +24,14 @@ class LogCtrl extends Controller
 
     public function getAll(){
         return Log::join('Doors', 'Logs.doorId', '=', 'Doors.id')
-            ->join('RfidCards', 'Logs.cardUid', '=', 'RfidCards.uid')
+            ->join('RfidCards', 'Logs.cardId', '=', 'RfidCards.uid')
             ->select('Doors.name as doorName', 'RfidCards.uid as cardUid', 'Logs.created_at as time', 'Logs.accessGranted')
             ->get();
     }
 
     public function getPastWeek(){
         return Log::join('Doors', 'Logs.doorId', '=', 'Doors.id')
-            ->join('RfidCards', 'Logs.cardUid', '=', 'RfidCards.uid')
+            ->join('RfidCards', 'Logs.cardId', '=', 'RfidCards.uid')
             ->select('Doors.name as doorName', 'RfidCards.uid as cardUid', 'Logs.created_at as time', 'Logs.accessGranted')
             ->where('logs.created_at', '>=', now()->subWeek())
             ->get();
@@ -39,7 +39,7 @@ class LogCtrl extends Controller
 
     public function getPastMonth(){
         return Log::join('Doors', 'Logs.doorId', '=', 'Doors.id')
-            ->join('RfidCards', 'Logs.cardUid', '=', 'RfidCards.uid')
+            ->join('RfidCards', 'Logs.cardId', '=', 'RfidCards.uid')
             ->select('Doors.name as doorName', 'RfidCards.uid as cardUid', 'Logs.created_at as time', 'Logs.accessGranted')
             ->where('logs.created_at', '>=', now()->subMonth())
             ->get();
@@ -47,7 +47,7 @@ class LogCtrl extends Controller
 
     public function getPastYear(){
         return Log::join('Doors', 'Logs.doorId', '=', 'Doors.id')
-            ->join('RfidCards', 'Logs.cardUid', '=', 'RfidCards.uid')
+            ->join('RfidCards', 'Logs.cardId', '=', 'RfidCards.uid')
             ->select('Doors.name as doorName', 'RfidCards.uid as cardUid', 'Logs.created_at as time', 'Logs.accessGranted')
             ->where('logs.created_at', '>=', now()->subYear())
             ->get();
